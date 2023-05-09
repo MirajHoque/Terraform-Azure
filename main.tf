67bda1e2d4ea9ed8fe131286fa15c14c0d185e42
Ashraful Hoque Miraj <ahmiraj@catec.ca>
2023-05-09 17:31:39 +0600

    storage account

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   //used to set provider source & version being used 
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.55.0"
+      source  = "hashicorp/azurerm"
+      version = "3.55.0"
     }
   }
 }
@@ -11,9 +11,9 @@ terraform {
 //Add cloud provider 
 provider "azurerm" {
   subscription_id = "d8026e85-c3a9-4a33-acb5-88d88c4daf6c"
-  client_id = "a34d4f82-4be8-4e70-8ecf-2ec7650f1f16"
-  client_secret = "K_G8Q~5CuEX7CHBJ57xaxvGmQJzqdvqWWqo~vb_w"
-  tenant_id = "cec0703b-aa1e-474f-a0b6-774a05568b47"
+  client_id       = "a34d4f82-4be8-4e70-8ecf-2ec7650f1f16"
+  client_secret   = "K_G8Q~5CuEX7CHBJ57xaxvGmQJzqdvqWWqo~vb_w"
+  tenant_id       = "cec0703b-aa1e-474f-a0b6-774a05568b47"
   #required
   features {
     //optional
@@ -21,7 +21,20 @@ provider "azurerm" {
 }
 
 //Resource Group
-resource "azurerm_resource_group" "rg_app" {
-  name     = "rg-app"
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
   location = "Canada Central"
+}
+
+//storage account 
+resource "azurerm_storage_account" "demo-sta" {
+  name                     = "demostorage32444"
+  resource_group_name      = "mtc-resources"
+  location                 = "Canada Central"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
 }
