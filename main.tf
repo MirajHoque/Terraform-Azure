a64e82f92b301b5c6eaa72c02ba9445868c13b9e
Ashraful Hoque Miraj <ahmiraj@catec.ca>
2023-05-29 17:24:23 +0600

    deploy web app and sql db

--- main.tf
+++ main.tf
@@ -32,41 +32,107 @@ resource "azurerm_resource_group" "mtc_rg" {
   location = local.location
 }
 
-#sql server
+#app service plan
+resource "azurerm_app_service_plan" "app_plan" {
+  name                = "app-serviceplan"
+  location            = azurerm_resource_group.mtc_rg.location
+  resource_group_name = azurerm_resource_group.mtc_rg.name
 
-resource "azurerm_sql_server" "mt_server" {
-  name                         = "mt-sqlserver"
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+  
+  depends_on = [ 
+    azurerm_resource_group.mtc_rg
+   ]
+}
+
+#azure web app
+resource "azurerm_app_service" "test_web" {
+  name                = "test-webapp32444"
+  location            = azurerm_resource_group.mtc_rg.location
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_plan.id
+
+  source_control {
+    repo_url           = "https://github.com/MirajHoque/ProductApp.git"
+    branch             = "master"
+    manual_integration = true
+    use_mercurial      = false
+  }
+
+  depends_on = [
+    azurerm_app_service_plan.app_plan
+  ]
+
+}
+
+#mssql server
+resource "azurerm_mssql_server" "mt_server" {
+  name                         = "mtserver32444"
   resource_group_name          = azurerm_resource_group.mtc_rg.name
   location                     = azurerm_resource_group.mtc_rg.location
   version                      = "12.0"
   administrator_login          = "sqladmin"
   administrator_login_password = "azure@123"
+}
+
+#mssql database
+resource "azurerm_mssql_database" "mt_db" {
+  name           = "mtdb"
+  server_id      = azurerm_mssql_server.mt_server.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+
+  depends_on = [
+    azurerm_mssql_server.mt_server
+  ]
+
   tags = {
-    environment = "production"
+    foo = "bar"
   }
 }
 
-#sql database
-resource "azurerm_sql_database" "mt_db" {
-  name                = "mt-db"
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  server_name         = azurerm_sql_server.mt_server.name
+
+#add firewall rule to the sql server
+resource "azurerm_sql_firewall_rule" "mt_server_firewall_rule_Azure_services" {
+  name                = "mt-server-firewall-rule-Azure-services"
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+  server_name         = azurerm_mssql_server.mt_server.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 
   depends_on = [
-    azurerm_sql_server.mt_server
+    azurerm_mssql_server.mt_server
   ]
 }
 
+
 #add firewall rule to the sql server
-resource "azurerm_sql_firewall_rule" "mt_server_firewall" {
-  name                = "mt-server-firewall"
+resource "azurerm_sql_firewall_rule" "mt_server_firewall_rule_Client_IP" {
+  name                = "mt-server-firewall-client-ip"
   resource_group_name = azurerm_resource_group.mtc_rg.name
-  server_name         = azurerm_sql_server.mt_server.name
+  server_name         = azurerm_mssql_server.mt_server.name
   start_ip_address    = "27.147.172.202"
   end_ip_address      = "27.147.172.202"
 
-  depends_on = [ 
-    azurerm_sql_server.mt_server
-   ]
+  depends_on = [
+    azurerm_mssql_server.mt_server
+  ]
+}
+
+#create table & add data to the table
+resource "null_resource" "database_table" {
+  provisioner "local-exec" {
+    command = "sqlcmd -S mtserver32444.database.windows.net -U sqladmin -P azure@123 -d mtdb -i init.sql"
+  }
+
+  depends_on = [
+    azurerm_mssql_server.mt_server
+  ]
 }
