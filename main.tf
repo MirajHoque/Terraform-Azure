e8db41db2e4f0d1e417add97e3c29c9fd704c6e7
Ashraful Hoque Miraj <51967822+MirajHoque@users.noreply.github.com>
2023-05-31 16:02:31 +0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 
 //Add cloud provider 
 provider "azurerm" {
-  subscription_id = "d8026e85-c3a9-4a33-acb5-88d88c4daf6c"
-  client_id       = "a34d4f82-4be8-4e70-8ecf-2ec7650f1f16"
-  client_secret   = "K_G8Q~5CuEX7CHBJ57xaxvGmQJzqdvqWWqo~vb_w"
-  tenant_id       = "cec0703b-aa1e-474f-a0b6-774a05568b47"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
   #required
   features {
     //optional
